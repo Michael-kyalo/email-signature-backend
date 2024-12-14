ef3459985375ee09e202ad8b43d9788a1ec3574c
Mike <kyalomike8@gmail.com>
2024-12-14 13:17:14 +0300

    fixed host url for swagger to use the render url

--- docs/docs.go
+++ docs/docs.go
@@ -518,9 +518,9 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:3000",
+	Host:             "email-signature-backend.onrender.com",
 	BasePath:         "",
-	Schemes:          []string{},
+	Schemes:          []string{"https"},
 	Title:            "Email Signature Generator API",
 	Description:      "API for managing email signatures with analytics",
 	InfoInstanceName: "swagger",
